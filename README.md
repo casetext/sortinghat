1cbc642f13a8a25099f9e08ae36bda9c8b96f26e
Josh Dague <daguej@email.uc.edu>
2015-04-02 11:43:26 -0700

    2.0: Remove lambda stuff
    The lambda invocation stuff didn't belong in this library.  It has been removed.
    
    - `watch()` and friends only accept callback functions now.
    - `invoke()` and `job()` are gone
    
    Also, sorting hat is no longer a singleton.  Instead of calling `init(firebase)`, this module now exports a `SortingHat` constructor.  Call it with a `Firebase`.

--- README.md
+++ README.md
@@ -3,15 +3,22 @@ The Sorting Hat
 
 The Sorting Hat sorts Firebase events into their Houses.  It accomplishes this through what we shall call magic.
 
-For muggles: This allows you to watch for new children added to a particular Firebase path and invoke either a callback function or an AWS Lambda function.
+For muggles: This allows you to watch for new children added to a particular Firebase path and invoke a callback function.
+
+    var SortingHat = require('sortinghat'),
+        hat = new SortingHat(new Firebase('https://example.firebaseio.com'));
+    
+    hat.watch('/foo', function(snap) {
+        console.log('new foo:', snap.val());
+    });
 
 Methods
 -------
 
-### `init(firebase)`
-Before using setting up watchers, you must pass in a `Firebase` instance.
+### `new SortingHat(firebase)`
+`require('sortinghat')` gives you this constructor.  Pass in a ready-to-use `Firebase` object.
 
-### `watch(path, [options,] cb)`
+### `watch(path, [options,] [cb])`
 Watch `path` for new children.  In order to not download all the data under this path, Sorting Hat requires you to use a well-defined ordering of data.  You cannot watch for children added at arbitrary locations; they **must** be added to the end of the order.
 
 - `options.order`
@@ -19,38 +26,21 @@ Watch `path` for new children.  In order to not download all the data under this
   - `priority` - Items will be added with a monotonically increasing priority.
   - *anything else* - Items will be objects containing this key, whose value will monotonically increase (ie a created timestamp).
 
-### `trigger(path, [options,] cb)`
+### `trigger(path, [options,] [cb])`
 Watch `path` for new children.  There are no constraints on order, however this requires that the Sorting Hat downloads **all** data from `path`.
 
 When calling a Lambda function, the new child will be automatically deleted from Firebase after Lambda accepts the invocation.
 
 **WARNING!** Only use `trigger` on a path where you plan on deleting the children shortly after they are created.  In other words, `path` should *usually* be empty.  If you ignore this advice, the Sorting Hat will use all your memory and/or crash.
 
-### `responder(path, [options,] cb)`
+### `responder(path, [options,] [cb])`
 Exactly like `trigger` -- watches `path` for new children with no constraints on order (and the associated caveats) -- except adds `/request` to the supplied `path`.
-
-### `job(fn[, args])`
-Sugar method.  Returns a function that takes a callback and always invokes `fn` with the same `args`.
-
-### `invoke(fn[, args][, cb])`
-Invokes an AWS Lambda function named `fn`, passing `args`.  Returns a promise and/or calls `cb` when Lambda accepts the invocation.  Tries 4 times with exponential backoff.
-
-### `prefix`
-
-`invoke()` prefixes the function name passed in with this string.  Useful when you're versioning functions with a prefix.
 
 `cb`
 ----
 
-The `cb` argument to `watch`, `trigger`, and `responder` can take either a function or a string.
+The `cb` argument to `watch`, `trigger`, and `responder` can optionally take  a callback function.
 
-- If you supply a function, it is invoked whenever there is a change.  The one and only argument passed to the function is the Firebase `Snapshot` of the new child.
-- If you supply a string, the AWS Lambda function named `cb` is invoked whenever there is a change.  The `data` object (first argument) in the lambda function contains:
-  - `path` - the full path to the new child.  ie `https://example.firebaseio.com/some/path/newchild`
-  - `key` - the key of the new child.  ie `newchild`
-  - `data` - the value of the new child.
+If you supply a function, it is invoked whenever there is a change.  The one and only argument passed to the function is the Firebase `Snapshot` of the new child.
 
-Production
-----------
-
-Sorting Hat will invoke AWS Lambda functions in AWS if `process.env.NODE_ENV == 'production'`.  Otherwise, it will invoke your lambda functions at `http://localhost:3033`.  Use [locavore](https://www.npmjs.com/package/locavore) to run something that looks like AWS Lambda on your machine.
+`watch`, `trigger`, and `responder` also return a `FirebaseWatcher`.  This object is an `EventEmitter` that emits `child_added` events for `watch` and `request` events for `trigger` and `responder`.
--- sortinghat.js
+++ sortinghat.js
@@ -1,53 +1,43 @@
 var EventEmitter = require('events').EventEmitter,
 	util = require('util'),
-	_ = require('lodash'),
-	Q = require('q'),
-	aws4 = require('aws4'),
-	request = require('request');
+	_ = require('lodash');
 
 var root, defaults = {
 	order: 'created'
-}, agentPool = {
-	maxSockets: 512
 };
 
-exports.init = function(firebase) {
-	exports.root = root = firebase;
-};
-
-var host = 'http://localhost:3033';
-if (process.env.NODE_ENV == 'production') {
-	host = 'https://lambda.us-east-1.amazonaws.com';
+function SortingHat(firebase) {
+	if (!(this instanceof SortingHat)) {
+		return new SortingHat(firebase);
+	}
+	if (!firebase || !firebase.root) {
+		throw new Error('Must provide the sorting hat with a Firebase reference');
+	}
+	this.root = firebase;
 }
+
+exports = module.exports = SortingHat;
 
 
 // things that can go wrong
 // - adding an item outside of the expected order = sadness
 // - adding the first item to an empty collection (nasty timeout hack?)
 // - deleting the last item causes last-1 item to fire again
-exports.watch = function(path, options, cb) {
+SortingHat.prototype.watch = function(path, options, cb) {
 	var watcher = new FirebaseWatcher();
-	if (!root) {
-		throw new Error('Must init() the sorting hat before calling watch()');
-	}
+	
 	if (typeof options == 'function' || typeof options == 'string') {
 		cb = options;
 		options = {};
 	}
 	if (typeof cb == 'function') {
 		watcher.on('child_added', cb);
-	} else if (typeof cb == 'string') {
-		watcher.on('child_added', function(snap) {
-			exports.invoke(cb, {
-				key: snap.key(),
-				path: snap.ref().toString()
-			});
-		});
 	}
+
 	options = _.assign({ path: path }, defaults, options);
 
 	
-	var ref = root.child(path);
+	var ref = this.root.child(path);
 
 	if (options.order == 'key') {
 		ref = ref.orderByKey();
@@ -72,119 +62,42 @@ exports.watch = function(path, options, cb) {
 	return watcher;
 };
 
-exports.trigger = function(path, options, cb) {
+SortingHat.prototype.trigger = function(path, options, cb) {
 	var watcher = new FirebaseWatcher();
-	if (!root) {
-		throw new Error('Must init() the sorting hat before calling trigger()');
-	}
+	
 	if (typeof options == 'function' || typeof options == 'string') {
 		cb = options;
 		options = {};
 	}
 	if (typeof cb == 'function') {
 		watcher.on('request', cb);
-	} else if (typeof cb == 'string') {
-		watcher.on('request', function(snap) {
-			exports.invoke(cb, {
-				path: path,
-				key: snap.key(),
-				data: snap.val()
-			}, function() {
-				snap.ref().set(null);
-			});
-		});
 	}
+
 	options = _.assign({ path: path }, defaults, options);
 
-	var ref = root.child(path);
+	var ref = this.root.child(path);
 	ref.on('child_added', function(snap) {
 		watcher.emit('request', snap);
 	});
 };
 
-exports.responder = function(path, options, cb) {
+SortingHat.prototype.responder = function(path, options, cb) {
 	var watcher = new FirebaseWatcher();
-	if (!root) {
-		throw new Error('Must init() the sorting hat before calling responder()');
-	}
+	
 	if (typeof options == 'function' || typeof options == 'string') {
 		cb = options;
 		options = {};
 	}
 	if (typeof cb == 'function') {
 		watcher.on('request', cb);
-	} else if (typeof cb == 'string') {
-		watcher.on('request', function(snap) {
-			exports.invoke(cb, {
-				path: path,
-				key: snap.key(),
-				data: snap.val()
-			}, function() {
-				snap.ref().set(null);
-			});
-		});
 	}
+
 	options = _.assign({ path: path }, defaults, options);
 
-	var ref = root.child(path).child('request');
+	var ref = this.root.child(path).child('request');
 	ref.on('child_added', function(snap) {
 		watcher.emit('request', snap);
 	});
-};
-
-exports.job = function(fn, args) {
-	args = args || {};
-	return function(cb) {
-		return exports.invoke(fn, args, cb);
-	};
-};
-
-exports.prefix = '';
-exports.invoke = function(fn, args, cb) {
-	var deferred = Q.defer(), tries = 0;
-	args = JSON.stringify(args);
-
-	function tryInvoke() {
-		var path = '/2014-11-13/functions/' + exports.prefix + fn + '/invoke-async/',
-			signature = aws4.sign({
-				service: 'lambda',
-				method: 'POST',
-				path: path,
-				headers: {
-					'Content-Type': 'application/json; charset=utf-8'
-				},
-				body: args
-			});
-
-
-		request({
-			method: 'POST',
-			url: host + path,
-			body: args,
-			pool: agentPool,
-			headers: signature.headers
-		}, function(err, res, body) {
-			if (err) {
-				failed(err);
-			} else if (res.statusCode != 202) {
-				failed(new Error('Invoke error ' + res.statusCode));
-			} else {
-				deferred.resolve();
-			}
-		});
-	}
-
-	function failed(err) {
-		if (tries++ > 3) {
-			deferred.reject(err);
-		} else {
-			setTimeout(tryInvoke, Math.pow(2, tries) * 1000);
-		}
-	}
-
-	tryInvoke();
-	deferred.promise.nodeify(cb);
-	return deferred.promise;
 };
 
 
